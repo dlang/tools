7abe27e2b3fcaefc394e9985ffbc0ec71e90cfb1
Andrei Alexandrescu <andrei@erdani.com>
2008-07-08 03:54:19 +0000

    Added copyright notice.

--- rdmd.d
+++ rdmd.d
@@ -1,3 +1,31 @@
+// Written in the D programming language.
+
+/*
+ *  Copyright (C) 2004-2006 by Digital Mars, www.digitalmars.com
+ *  Written by Andrei Alexandrescu, www.erdani.org
+ *
+ *  This software is provided 'as-is', without any express or implied
+ *  warranty. In no event will the authors be held liable for any damages
+ *  arising from the use of this software.
+ *
+ *  Permission is granted to anyone to use this software for any purpose,
+ *  including commercial applications, and to alter it and redistribute it
+ *  freely, subject to the following restrictions:
+ *
+ *  o  The origin of this software must not be misrepresented; you must not
+ *     claim that you wrote the original software. If you use this software
+ *     in a product, an acknowledgment in the product documentation would be
+ *     appreciated but is not required.
+ *  o  Altered source versions must be plainly marked as such, and must not
+ *     be misrepresented as being the original software.
+ *  o  This notice may not be removed or altered from any source
+ *     distribution.
+ */
+
+/*
+  See $(WEB digitalmars.com/d/2.0/rdmd.html) for documentation.
+*/
+
 import std.getopt, std.string, std.process, std.stdio, std.contracts, std.file,
     std.algorithm, std.iterator, std.md5, std.path, std.regexp, std.getopt,
     std.c.stdlib, std.date, std.process;
@@ -39,7 +67,8 @@ int main(string[] args)
         bailout = true;
         auto browser = std.process.getenv("BROWSER");
         foreach (b; [ browser, "firefox",
-                        "sensible-browser", "x-www-browser" ]) {
+                        "sensible-browser", "x-www-browser" ])
+        {
             if (!b.length) continue;
             if (!system(b~" http://www.digitalmars.com/d/2.0/rdmd.html"))
                 return;
