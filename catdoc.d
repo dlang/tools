01938eb233d551283cbd1a979fb2066a0cedfc86
Walter Bright <walter@walterbright.com>
2011-07-03 20:41:58 -0700

    concateniate ddoc files

---
+++ catdoc.d
@@ -0,0 +1,60 @@
+
+import std.stdio;
+import std.getopt;
+
+int main(string[] args)
+{
+    if (args.length < 2)
+    {
+	writeln("catdoc: Concatenate Ddoc files
+Usage:
+    catdoc -o=outputfile sourcefiles...
+");
+	return 1;
+    }
+
+    string ofile;
+    getopt(args, "o", &ofile);
+    if (!ofile)
+    {
+	writeln("catdoc: set output file with -o=filename");
+	return 1;
+    }
+    if (args.length < 2)
+    {
+	writeln("catdoc: no input files");
+	return 1;
+    }
+
+    string comment = "Ddoc\n";
+    string macros;
+    foreach (arg; args[1..$])
+    {
+	//writeln(arg);
+	string input = cast(string)std.file.read(arg);
+	if (input.length < 4 || input[0..4] != "Ddoc")
+	{   writefln("catdoc: %s is not a Ddoc file", arg);
+	    return 1;
+	}
+	foreach (i, c; input)
+	{
+	    if (c == '\n')
+	    {
+		if (i + 8 < input.length && std.string.icmp(input[i + 1 .. i + 8], "Macros:") == 0)
+		{
+		    comment ~= input[4 .. i + 1];
+		    if (!macros)
+			macros = "Macros:\n";
+		    macros ~= input[i + 8 .. $];
+		    goto L1;
+		}
+	    }
+	}
+	comment ~= input[4 .. $];
+    L1: ;
+    }
+
+    std.file.write(ofile, comment ~ macros);
+
+    return 0;
+}
