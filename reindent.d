9e1bd3261a92c0d239f22d270e80c2a7883c9251
H. S. Teoh <hsteoh@quickfur.ath.cx>
2015-02-11 09:52:25 -0800

    Add copyright notice.

--- reindent.d
+++ reindent.d
@@ -1,3 +1,12 @@
+#!/usr/bin/env rdmd
+
+/* Distributed under the Boost Software License, Version 1.0.
+ *    (See accompanying file LICENSE_1_0.txt or copy at
+ *          http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+module reindent;
+
 import std.range.primitives;
 
 /**
